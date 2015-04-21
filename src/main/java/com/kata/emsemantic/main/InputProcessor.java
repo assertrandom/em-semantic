3df4fe382bd8f68331c33ca5b29130e3dc716560
Manav Leslie <manav.leslie@stratifiedmedical.com>
2015-04-21 06:26:26 +0100

    added initial queryprocessor

---
+++ src/main/java/com/kata/emsemantic/main/InputProcessor.java
@@ -0,0 +1,19 @@
+package com.kata.emsemantic.main;
+
+import com.kata.emsemantic.main.service.QueryProcessor;
+import com.kata.emsemantic.main.service.QueryProcessorImpl;
+
+import java.util.Scanner;
+
+/**
+ * Created by manavleslie on 21/04/15.
+ */
+public class InputProcessor {
+
+    public static void main(String[] args) {
+
+        QueryProcessor queryProcessor  = new QueryProcessorImpl();
+
+
+    }
+}
---
+++ src/main/java/com/kata/emsemantic/main/service/QueryProcessorImpl.java
@@ -0,0 +1,7 @@
+package com.kata.emsemantic.main.service;
+
+/**
+ * Created by manavleslie on 21/04/15.
+ */
+public class QueryProcessorImpl implements  QueryProcessor {
+}
